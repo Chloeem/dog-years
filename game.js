6aeaa3eab3d1d0da76b8a874926b81f21ff6646b
Chloé Stokes <50859876+Chloeem@users.noreply.github.com>
2020-10-24 17:17:00 +0100

    Create game.js

---
+++ game.js
@@ -0,0 +1,28 @@
+// My current age
+let myAge = 4;
+
+// A dogs first 2 years
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+// Subtracting first 2 years
+let laterYears = myAge - 2;
+
+// Amount of dog years for later year in life
+laterYears *= 4;
+
+/*
+console.log(earlyYears);
+console.log(laterYears);
+*/
+
+// My age in dog years 
+myAgeInDogYears = earlyYears + laterYears;
+
+// Storing my name in lowercase
+const myName = "Tsuki".toLowerCase();
+
+// String to explain outcome
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+
